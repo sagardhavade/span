6ab763c74bfe0178d542aee06b4d6ab9b89bd2b6
= <=>
2023-11-24 14:44:25 +0530

    Initial commit on main branch

--- application/controllers/Testcontroller.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class Testcontroller extends CI_Controller {
-
-	function __construct() {
-		echo "string"; die;
-    }
-}
--- application/logs/log-2022-01-25.php
+++
@@ -1,4 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2022-01-25 14:46:38 --> 404 Page Not Found: Vaibhav%20project/Sop
-ERROR - 2022-01-25 14:47:13 --> 404 Page Not Found: Vaibhav%20project/Sop
